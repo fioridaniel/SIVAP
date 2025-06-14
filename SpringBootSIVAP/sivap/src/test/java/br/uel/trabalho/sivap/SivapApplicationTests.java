766306ad2a8ee94a4ef99387345b240dd38d6e17
fiori_daniel <daniel.fiori.souza@uel.br>
2025-06-14 15:45:47 -0300

    feat: projeto spring boot adicionado ao sivap

---
+++ SpringBootSIVAP/sivap/src/test/java/br/uel/trabalho/sivap/SivapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uel.trabalho.sivap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
