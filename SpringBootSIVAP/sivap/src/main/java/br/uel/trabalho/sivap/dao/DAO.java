708f914361d4c7750aa5305579799c9d311cd91b
pedroschettini <pedroschettini213@gmail.com>
2025-06-17 20:10:36 -0300

    feat: Adiciona estrutura de conexão com o banco de dados

---
+++ SpringBootSIVAP/sivap/src/main/java/br/uel/trabalho/sivap/dao/DAO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @param <ENTITY>
+ */
+public interface DAO<ENTITY> {
+
+    public void create(ENTITY t)
+            throws SQLException;
+    public ENTITY read(Integer id)
+            throws SQLException;
+    public void update(ENTITY t)
+            throws SQLException;
+    public void delete(Integer id)
+            throws SQLException;
+    public List<ENTITY> all()
+            throws SQLException;
+
+}
