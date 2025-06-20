ad68b589f50840527b4051cdf63ee1379150bce5
pedroschettini <pedroschettini213@gmail.com>
2025-06-20 19:02:53 -0300

    feat: CRUD para produtor rural

---
+++ SpringBootSIVAP/sivap/src/main/java/br/uel/trabalho/sivap/dao/PgProdutorRuralDAO.java
@@ -0,0 +1,107 @@
+package dao;
+
+import jdbc.ConnectionFactory;
+import model.ProdutorRural;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PgProdutorRuralDAO implements ProdutorRuralDAO {
+
+    @Override
+    public ProdutorRural inserir(ProdutorRural produtor) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "INSERT INTO produtores_rurais (cpf, nome, sexo, dt_nasc, endereco) VALUES (?, ?, ?, ?, ?);";
+
+        // Usando try-with-resources para garantir que a conexão e o statement sejam fechados.
+        try (Connection conn = ConnectionFactory.getInstance().getConnection();
+             PreparedStatement pst = conn.prepareStatement(sql)) {
+
+            pst.setInt(1, produtor.getCpf());
+            pst.setString(2, produtor.getNome());
+            pst.setString(3, String.valueOf(produtor.getSexo())); // Converte char para String para o JDBC
+            pst.setDate(4, new java.sql.Date(produtor.getDt_nasc().getTime())); // Converte java.util.Date para java.sql.Date
+            pst.setString(5, produtor.getEndereco());
+
+            pst.executeUpdate();
+        }
+        return produtor;
+    }
+
+    @Override
+    public ProdutorRural buscarPorCpf(String cpf) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT * FROM produtores_rurais WHERE cpf = ?;";
+        ProdutorRural produtor = null;
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection();
+             PreparedStatement pst = conn.prepareStatement(sql)) {
+
+            pst.setInt(1, cpf);
+
+            try (ResultSet rs = pst.executeQuery()) {
+                if (rs.next()) {
+                    String nome = rs.getString("nome");
+                    char sexo = rs.getString("sexo").charAt(0);
+                    java.util.Date dtNasc = rs.getDate("dt_nasc");
+                    String endereco = rs.getString("endereco");
+
+                    produtor = new ProdutorRural(cpf, nome, sexo, dtNasc, endereco);
+                }
+            }
+        }
+        return produtor;
+    }
+
+    @Override
+    public List<ProdutorRural> listarTodos() throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT * FROM produtores_rurais ORDER BY nome;";
+        List<ProdutorRural> produtores = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                String cpf = rs.getString("cpf");
+                String nome = rs.getString("nome");
+                char sexo = rs.getString("sexo").charAt(0);
+                java.util.Date dtNasc = rs.getDate("dt_nasc");
+                String endereco = rs.getString("endereco");
+
+                ProdutorRural produtor = new ProdutorRural(cpf, nome, sexo, dtNasc, endereco);
+                produtores.add(produtor);
+            }
+        }
+        return produtores;
+    }
+
+    @Override
+    public void atualizar(ProdutorRural produtor) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE produtores_rurais SET nome = ?, sexo = ?, dt_nasc = ?, endereco = ? WHERE cpf = ?;";
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection();
+             PreparedStatement pst = conn.prepareStatement(sql)) {
+
+            pst.setString(1, produtor.getNome());
+            pst.setString(2, String.valueOf(produtor.getSexo()));
+            pst.setDate(3, new java.sql.Date(produtor.getDt_nasc().getTime()));
+            pst.setString(4, produtor.getEndereco());
+            pst.setInt(5, produtor.getCpf());
+
+            pst.executeUpdate();
+        }
+    }
+
+    @Override
+    public void deletar(String cpf) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM produtores_rurais WHERE cpf = ?;";
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection();
+             PreparedStatement pst = conn.prepareStatement(sql)) {
+
+            pst.setInt(1, cpf);
+            pst.executeUpdate();
+        }
+    }
+}
