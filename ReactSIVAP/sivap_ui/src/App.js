39980b238c4dda63f6d8b85435437983f8fbc901
pedroschettini <pedroschettini213@gmail.com>
2025-06-27 19:48:24 -0300

    feat: adição de arquivos de configuração e implementação de componentes básicos no front-end com React, incluindo Login e rotas.

--- ReactSIVAP/sivap_ui/src/App.js
+++ ReactSIVAP/sivap_ui/src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./Routes";
 
 function App() {
-  const [dado, setDado] = useState('');
-  
-  useEffect(() => {
-    /* os then sao encadeados. a ordem importa */
-      fetch('http://localhost:8080/produtores')
-      .then(response => response.json())
-      .then(data => { 
-        console.log(data[0].cpf);
-        setDado(data[0].cpf); 
-      })
-      .catch(error => console.error('Erro ao buscar mensagem:', error));
-  }, []);
-
   return (
-    <h1>Mensagem do server: {dado}</h1>
-  )
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
 }
 
 export default App;
