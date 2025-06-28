058ccdfe093b9091604cfee23c6ebe7ba7183dbd
fiori_daniel <daniel.fiori.souza@uel.br>
2025-06-28 15:13:52 -0300

    feat: log em PropriedadeController para debugar. posteiormente adicionar nas outras entidades. melhorias em Propriedade.js

--- ReactSIVAP/sivap_ui/src/screens/Propriedade.js
+++ ReactSIVAP/sivap_ui/src/screens/Propriedade.js
@@ -1,62 +1,67 @@
-import { useEffect, useState } from "react"
+import './App.css';
+import { useState } from 'react';
 
-const Propriedade = () => {
-    const [nome, setNome] = useState('');
-    const [area, setArea] = useState('');
+function App() {
+  const [nome, setNome] = useState('');
+  const [area, setArea] = useState('');
 
-    /* nao sei se -1 eh o melhor para inicializar */
-    const [latitude, setLatitude] = useState(-1);
-    const [longitude, setLongitude] = useState(-1);
-    
-    const sendForm = () => {
-        if(!nome || !area || !latitude || !longitude) {
-            alert("Preencha todos os campos para enviar os dados");
-        }
+  /* nao sei se eh o melhor para inicializar numeros */
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  
+  const sendForm = async (event) => {
+    event.preventDefault(); /* evita o recarregamento completo da pagina para nao quebrar o react */
 
-        /* fazer requisicao */
-        try {
-            const response = fetch('url', {
-                method:'POST',
-                body: JSON.stringify( {
-                    nome: nome,
-                    area: area,
-                    latitude: latitude,
-                    longitude: longitude
-                })
-            })
-            console.log("reposta do server: " + response);
-        }
-
-        catch(error) {
-            console.log("erro ao processar request: " + error);
-        }
+    if(!nome || !area || !latitude || !longitude) {
+      alert("Preencha todos os campos para enviar os dados");
     }
 
-    return (
-        <form onSubmit={sendForm}>
-            <label>
-            Nome:
-            <input type="text" onChange={(e) => setNome(e.target.value)} />
-            </label>
+    /* fazer requisicao */
+    try {
+      const response = await fetch('http://localhost:8080/propriedades', {
+        method:'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nome: nome,
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+          area: parseFloat(area)
+        })
+      })
+    }
 
-            <label>
-            area:
-            <input type="text" onChange={(e) => setArea(e.target.value)} />
-            </label>        
-            
-            <label>
-            lat:
-            <input type="text" onChange={(e) => setLatitude(e.target.value)} />
-            </label>        
-            
-            <label>
-            long:
-            <input type="text" onChange={(e) => setLongitude(e.target.value)} />
-            </label>        
-            
-            <input type="submit" value="Submit" />
-        </form>
-    );
-
-    /* pegar dados do formulario e setar estados. depois disso, colocar no banco */
+    catch(error) {
+      console.log("erro ao processar request: " + error);
+    }
 }
+
+  return (
+    <form onSubmit={sendForm}>
+      <label>
+        Nome:
+        <input type="text" onChange={(e) => setNome(e.target.value)} />
+      </label>
+
+      <label>
+        area:
+        <input type="text" onChange={(e) => setArea(e.target.value)} />
+      </label>        
+      
+      <label>
+        lat:
+        <input type="text" onChange={(e) => setLatitude(e.target.value)} />
+      </label>        
+      
+      <label>
+        long:
+        <input type="text" onChange={(e) => setLongitude(e.target.value)} />
+      </label>        
+      
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default App;
